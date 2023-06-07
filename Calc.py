7eb16924db54658f6b51a91537a7b773308f6457
TobiasKaes13 <tobias.14kaes@gmail.com>
2023-06-07 20:09:02 +0200

    Create Calc.py

---
+++ Calc.py
@@ -0,0 +1,71 @@
+# Definition der Funktionen für die vier Grundrechenarten
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Fehler da du durch 0 geteilt hast"
+
+
+# Begrüßungsnachricht
+
+print("Willkommen zum Taschenrechner!")
+
+# Hauptschleife, um Benutzerinteraktion fortzusetzen
+
+while True:
+    # Menüoptionen anzeigen
+
+    print("Bitte wählen Sie eine Operation:")
+    print("1. Addition")
+    print("2. Subtraktion")
+    print("3. Multiplikation")
+    print("4. Division")
+    print("5. Beenden")
+
+    # Benutzereingabe für die gewählte Option
+
+    choice = input("deine Auswahl: ")
+
+    # Beenden, wenn Option "5" ausgewählt wurde
+
+    if choice == "5":
+        print("Taschenrechner wird beendet.")
+        break
+
+    # Benutzereingabe für die beiden Zahlen
+
+    num1 = float(input("Geben Sie die erste Zahl ein: "))
+    num2 = float(input("Geben Sie die zweite Zahl ein: "))
+
+    # Variablendefinition für das Ergebnis
+
+    result = None
+
+    # Durchführen der gewählten Operation
+
+    if choice == "1":
+        result = add(num1, num2)
+    elif choice == "2":
+        result = subtract(num1, num2)
+    elif choice == "3":
+        result = multiply(num1, num2)
+    elif choice == "4":
+        result = divide(num1, num2)
+
+    # Ausgabe des Ergebnisses, sofern vorhanden
+
+    if result is not None:
+        print("Ergebnis:", result)
